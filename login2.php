00957d3201891b53242aa2ad830af969af293b50
jemmiechen <105255131+jemmiechen@users.noreply.github.com>
2024-07-19 22:37:52 +0800

    Add files via upload

---
+++ login2.php
@@ -0,0 +1,11 @@
+<?
+include("check.php"); 
+?>
+
+<html><head>
+<meta http-equiv="Content-Type" content="text/html; charset=big5">
+</head><body>
+<body bgcolor="#FFFFCC" text="#000000" leftmargin="0" topmargin="20" marginwidth="0" marginheight="0">
+<p align="center"><font color="#FF0000">
+您已經通過認證</font></p>
+</body></html>
